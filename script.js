9178a22b69bd9cbe949191af0f6dc369e90ded84
VipersEye <viperseye@mail.ru>
2024-02-04 19:03:03 +0300

    feat: solve JS katas

--- script.js
+++ script.js
@@ -84,3 +84,95 @@ function consecutiveNums(arr, groupLen) {
   }
   return freeSpace.every((item) => item === 0);
 }
+
+// https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/javascript
+// * 8 kyu
+
+const phrases = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
+const howMuchILoveYou = (nbPetals) => phrases[(nbPetals - 1) % 6];
+
+// https://www.codewars.com/kata/5763bb0af716cad8fb000580/train/javascript
+// * 8 kyu
+
+const countSquares = (cuts) => cuts === 0 ? 1 : (cuts + 1) ** 3 - (cuts - 1) ** 3;
+
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript
+// * 8 kyu
+
+const summation = (n) => n === 0 ? n : n + summation(n - 1);
+
+// https://www.codewars.com/kata/58d248c7012397a81800005c/train/javascript
+// * 8 kyu
+
+const cubeChecker = (volume, side) => side > 0 && volume > 0 && volume === side ** 3;
+
+// https://www.codewars.com/kata/5748a883eb737cab000022a6/train/javascript
+// * 8 kyu
+
+const cannonsReady = (gunners) => Object.values(gunners).every((phrase) => phrase === 'aye') ? 'Fire!' : 'Shiver me timbers!';
+
+// https://www.codewars.com/kata/5bc73331797b005d18000255/train/javascript
+// * 7 kyu
+
+const legs = [['^', '^'], ['/\\', '/\\'], ['/╲', '╱\\'], ['╱╲', '╱╲']];
+const drawSpider = (legSize, bodySize, mouth, eye) => `${legs[legSize - 1][0]}${'('.repeat(bodySize)}${eye.repeat(2 ** (bodySize - 1))}${mouth}${eye.repeat(2 ** (bodySize - 1))}${')'.repeat(bodySize)}${legs[legSize - 1][1]}`;
+
+// https://www.codewars.com/kata/5a8bcd980025e99381000099/train/javascript
+// * 7 kyu
+
+const squareUp = (n) => new Array(n).fill(new Array(2).fill(0)).map(([zeros, nums], i) => [new Array(n - i - 1).fill(0), [...new Array(i + 1).keys()].map((_, index) => index + 1).reverse()]).flat();
+
+// https://www.codewars.com/kata/62c93765cef6f10030dfa92b/train/javascript
+// * 7 kyu
+
+const solution = (start, finish) => finish - start < 4 ? finish - start : Math.floor((finish - start) / 3) + (finish - start) % 3;
+
+// https://www.codewars.com/kata/659af96994b858db10e1675f/train/javascript
+// * 6 kyu
+
+const beginning = function findBeginning(xs) {
+  let beginning;
+  for (let i = 1; i < xs.length; i += 1) {
+    beginning = +xs.slice(0, i);
+    let next = beginning + 1;
+    let flag = true;
+    for (let j = i; j < xs.length; next += 1, j += String(next).length - (String(next).length - String(next - 1).length)) {
+      if (next != xs.slice(j, j + String(next).length)) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) return beginning;
+    if (String(beginning).length > xs.length - String(beginning).length) break;
+  }
+  return +xs;
+};
+
+// https://www.codewars.com/kata/5539fecef69c483c5a000015/train/javascript
+// * 6 kyu
+
+const backwardsPrime = function generatePrimes(start, stop) {
+  const primes = [];
+  const allPrimes = new Set();
+
+  const isPrime = function definePrime(n, nR) {
+    if (n === nR) return false;
+    const max = Math.ceil(Math.max(n, nR) ** 0.5);
+    for (let i = 2; i <= max; i += 1) {
+      if (n % i === 0 || (nR % i === 0 && i !== n && i !== nR)) {
+        return false;
+      }
+    }
+    allPrimes.add(n);
+    allPrimes.add(nR);
+    return true;
+  };
+
+  for (let i = start < 13 ? 13 : start; i <= stop; i += 1) {
+    if (allPrimes.has(i)) {
+      primes.push(i);
+    } else if (isPrime(i, +String(i).split('').reverse().join(''))) primes.push(i);
+  }
+
+  return primes;
+};
